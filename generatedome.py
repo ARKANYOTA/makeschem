c0b94c031479fc71362627e636279570a925e6aa
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2022-02-13 19:49:30 +0100

    Block num of blocks

--- generatedome.py
+++ generatedome.py
@@ -5,11 +5,15 @@ if __name__ == "__main__":
     center = 43.5
     rayon = 43
     plusoumoins = 43
+    print("Calculating...")
     f = open("a.scdef", "w")
     for x in range(taille):
         for y in range(taille):
             for z in range(taille):
                 if (rayon**2)-plusoumoins <round((x-center)**2+(y)**2+(z-center)**2, 2)<=(rayon**2)+plusoumoins:
                     f.write(f"{x} {y} {z} glass\n")
+                    n += 1
     f.close()
-
+    print(f"Blocks: {n}")
+    print(f"Stacks: {n//64} + {n%64} items")
+    print(f"Shulker Boxes: {n//(64*27)} + {(n//64)%27} stacks + {(n%64)} items")
